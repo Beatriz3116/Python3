e1bfaf48210cff59f0754ee11ac6298eb6012cda
Beatriz Sanchez <bea.trish.3116@gmail.com>
2026-09-23 14:18:32 +0200

    datos simples

---
+++ Datos Simples/Ejercicio 5.py
@@ -0,0 +1,3 @@
+horas = int(input('Introduce las horas:'))
+paga = int(input('Introduce tu paga por hora:'))
+print(f"Tu salario es de: {horas * paga}") 
---
+++ Datos Simples/Ejercicio 6.py
@@ -0,0 +1,3 @@
+num = int(input('Introduce un número:'))
+suma = (num * (num + 1)) / 2
+print(f"La suma de los primeros enteros desde 1 hasta {num} es: {suma}")
