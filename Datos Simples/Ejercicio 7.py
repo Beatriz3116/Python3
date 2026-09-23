c2eef2757091a29a6dc9414d79c1434778157e99
Beatriz Sanchez <bea.trish.3116@gmail.com>
2026-09-23 14:23:35 +0200

    datos simples

---
+++ Datos Simples/Ejercicio 7.py
@@ -0,0 +1,5 @@
+peso = float(input("Ingrese su peso en kilogramos: "))
+estatura = float(input("Ingrese su estatura en metros: "))
+imc = peso / (estatura ** 2)
+imc_redondeado = round(imc, 2)
+print("Tu índice de masa corporal (IMC) es:", imc_redondeado)
