6d84752a4fd538a001ac50c54656f473a62cbbea
Beatriz Sanchez <bea.trish.3116@gmail.com>
2026-09-24 12:38:25 +0200

    datos simples

---
+++ Datos Simples/Ejercicio 10.py
@@ -0,0 +1,6 @@
+payasos = int(input('Número de payasos vendidos: '))
+muñecas = int(input('Número de muñecas vendidas: '))
+
+total_pagos = (payasos * 112) + (muñecas * 75)
+
+print('El peso total del paquete es:', total_pagos, 'g')
