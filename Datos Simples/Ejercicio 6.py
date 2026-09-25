02409ea4f11be6df9618a2cf01d3b4f2972557a4
Beatriz Sanchez <bea.trish.3116@gmail.com>
2026-09-25 11:59:54 +0200

    datos simples

--- Datos Simples/Ejercicio 6.py
+++ Datos Simples/Ejercicio 6.py
@@ -1,3 +1,3 @@
 num = int(input('Introduce un número:'))
 suma = (num * (num + 1)) / 2
-print(f"La suma de los primeros enteros desde 1 hasta {num} es: {suma}")
+print(f'La suma de los primeros enteros desde 1 hasta {num} es: {suma}')
